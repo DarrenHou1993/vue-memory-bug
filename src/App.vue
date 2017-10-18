2e91f0e11ba5a17292440bb33a3446052acd2af9
pillarhou <pillarhou@yeah.net>
2017-10-18 12:46:00 +0800

    bug code

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <router-link to="/">/hello</router-link>
+    <router-link to="/home">/home</router-link>
+    <keep-alive include="Hello">
+      <router-view></router-view>
+    </keep-alive>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'app',
+  data() {
+    return {
+    }
+  },
+  created() {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div v-for="item in items">{{item}}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    data() {
+        return {
+            items: []
+        }
+    },
+    mounted() {
+        console.log('in')
+        for (let i = 0; i < 2000; i++) {
+            this.items.push(i)
+        }
+    }
+}
+</script>
+
